5b6c14f5a89a7c33fac8eb80644bda287133189f
arhandev <arhandev.code@gmail.com>
2022-03-13 17:53:46 +0700

    feat: add obstacle level 1

--- index.js
+++ index.js
@@ -18,11 +18,42 @@ let score = 0;
 
 // declare assets
 let lifeImg = new Image();
+let obstacleImg = new Image();
 lifeImg.src = './assets/life.png';
+obstacleImg.src = './assets/obstacle.png';
 let audioGameOver = new Audio();
 let audioAppleBite = new Audio();
 audioGameOver.src = 'assets/game-over.mp3';
 audioAppleBite.src = 'assets/apple-bite.mp3';
+
+let dataObstacle = [];
+
+let ObstaclePerLevel = {
+	1: [
+		{
+			x: 3,
+			y: 14,
+			length: 24,
+			direction: 'horizontal',
+		},
+	],
+};
+
+let snake1 = initSnake();
+
+let apples = [
+	{
+		position: initPosition(),
+	},
+	{
+		position: initPosition(),
+	},
+];
+
+let health = {
+	appear: false,
+	position: initPosition(),
+};
 
 function levelUp() {
 	level++;
@@ -69,21 +100,6 @@ function initSnake() {
 		direction: initDirection(),
 	};
 }
-let snake1 = initSnake();
-
-let apples = [
-	{
-		position: initPosition(),
-	},
-	{
-		position: initPosition(),
-	},
-];
-
-let health = {
-	appear: false,
-	position: initPosition(),
-};
 
 function updateLifeHtml() {
 	let lifeElement = document.getElementById('life');
@@ -107,6 +123,29 @@ function drawScore(snake) {
 	scoreCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 	scoreCtx.font = '30px Arial';
 	scoreCtx.fillText(score, 10, scoreCanvas.scrollHeight / 2);
+}
+
+function obstacle(ctx) {
+	let currentObstacle = ObstaclePerLevel[level];
+	currentObstacle.forEach(obstacle => {
+		drawObstacle(ctx, obstacle.x, obstacle.y, obstacle.length, obstacle.direction);
+	});
+}
+
+function drawObstacle(ctx, x, y, length, direction) {
+	let tmpArray = [];
+	if (direction === 'vertical') {
+		for (let i = y; i < y + length; i++) {
+			tmpArray.push({ x: i, y: 13 });
+			drawImagePixel(ctx, x, i, obstacleImg);
+		}
+	} else if (direction === 'horizontal') {
+		for (let i = x; i < x + length; i++) {
+			tmpArray.push({ x: i, y: y });
+			drawImagePixel(ctx, i, y, obstacleImg);
+		}
+	}
+	dataObstacle.push(tmpArray);
 }
 
 function draw() {
@@ -138,6 +177,8 @@ function draw() {
 		if (health.appear) {
 			drawImagePixel(ctx, health.position.x, health.position.y, lifeImg);
 		}
+
+		obstacle(ctx);
 
 		drawScore(snake1);
 	}, REDRAW_INTERVAL);
@@ -241,6 +282,17 @@ function checkCollision(snakes) {
 			}
 		}
 	}
+
+	for (let i = 0; i < snakes.length; i++) {
+		for (let j = 0; j < dataObstacle.length; j++) {
+			for (let k = 0; k < dataObstacle[j].length; k++) {
+				if (snakes[i].head.x == dataObstacle[j][k].x && snakes[i].head.y == dataObstacle[j][k].y) {
+					isCollide = true;
+				}
+			}
+		}
+	}
+
 	if (isCollide) {
 		audioGameOver.play();
 		alert('Game over');
